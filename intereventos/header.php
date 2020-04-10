e01be6060f0311f5adaa5b453987eda716d5d61a
Lucas Consa <lucaslssouza2015@gmail.com>
2020-04-09 22:52:41 -0300

    mudando tela inicial

--- intereventos/header.php
+++ intereventos/header.php
@@ -11,11 +11,19 @@ session_start();
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/css/bootstrap.css">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css">
   <link rel="stylesheet" href="css/css/nav.css">
-  <link rel="stylesheet" href="node_modules/bootstrap/compiler/bootstrap.css">
   <link rel="stylesheet" href="css/css/style.css">
-  <!-- <link rel="stylesheet" type="text/css" href="node_modules/font-awesome/css/font-awesome.css"> -->
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
   <title>Inter Eventos</title>
 </head>
-<body>
+<body style="background: #212529;">
+  <div class="fundo-logo">
+    <div>
+      <div class="d-flex justify-content-center p-3">
+        <a href="index.php" class="text-center">
+          <img src="imagem/slidePronto.jpg" width="50%" class="img img-responsive" alt="Inter Eventos" >
+        </a>
+      </div>
+    </div>
+    <hr class="bg-light" style="width:75%;">
     <?php require 'nav.php'?>
+  </div>
--- intereventos/nav.php
+++ intereventos/nav.php
@@ -37,144 +37,40 @@
 </div>
 </nav> -->
 
-<div class="page-wrapper chiller-theme">
-  <a id="show-sidebar" class="btn btn-sm btn-dark" href="#">
-    <i class="fas fa-bars"></i>
-  </a>
-  <nav id="sidebar" class="sidebar-wrapper">
-    <div class="sidebar-content">
-      <div class="sidebar-brand"> 
-        <!-- <a href="#">A festa começa AQUI </a> -->
-        <div id="close-sidebar" style="text-align: end;width: 100%;">
-          <i class="fas fa-times"></i>
-        </div>
-      </div>
-      <div class="sidebar-header">
-        <div class="user-pic">
-          <img class="img-responsive img-rounded" src="https://raw.githubusercontent.com/azouaoui-med/pro-sidebar-template/gh-pages/src/img/user.jpg"
-            alt="User picture">
-        </div>
-        <div class="user-info">
-          <span class="user-name">Jhon
-            <strong>Smith</strong>
-          </span>
-          <span class="user-role">Administrator</span>
-          <span class="user-status">
-            <i class="fa fa-circle"></i>
-            <span>Online</span>
-          </span>
-        </div>
-      </div>
-      <!-- sidebar-header  -->
-      <div class="sidebar-menu">
-        <ul>
-          <li class="header-menu">
-            <span> Geral </span>
-          </li>
-          <li>
-            <a href="index.php">
-              <i class="fa fa-tachometer-alt"></i>
-              <span>Dashboard</span>
-              <span class="badge badge-pill badge-warning">New</span>
-            </a>
-          </li>
-          <?php if (isLoggedIn() == true) { ?>
-          <li>
-            <a href="index.php">
-              <i class="fa fa-tachometer-alt"></i>
-              <span>Perfil</span>
-              <span class="badge badge-pill badge-warning">New</span>
-            </a>
-          </li>
-          <?php } ?>
-          <li class="sidebar-dropdown">
-            <a href="#">
-              <i class="far fa-gem"></i>
-              <span>Baladas</span>
-            </a>
-            <div class="sidebar-submenu">
-              <ul>
-                <li>
-                  <a href="#">General</a>
-                </li>
-                <li>
-                  <a href="#">Panels</a>
-                </li>
-                <li>
-                  <a href="#">Tables</a>
-                </li>
-                <li>
-                  <a href="#">Icons</a>
-                </li>
-                <li>
-                  <a href="#">Forms</a>
-                </li>
-              </ul>
-            </div>
-          </li>
-          <li class="sidebar-dropdown">
-            <a href="#">
-              <i class="fa fa-chart-line"></i>
-              <span>Charts</span>
-            </a>
-            <div class="sidebar-submenu">
-              <ul>
-                <li>
-                  <a href="#">Pie chart</a>
-                </li>
-                <li>
-                  <a href="#">Line chart</a>
-                </li>
-                <li>
-                  <a href="#">Bar chart</a>
-                </li>
-                <li>
-                  <a href="#">Histogram</a>
-                </li>
-              </ul>
-            </div>
-          </li>
-          <li class="sidebar-dropdown">
-            <a href="#">
-              <i class="fa fa-globe"></i>
-              <span>Maps</span>
-            </a>
-            <div class="sidebar-submenu">
-              <ul>
-                <li>
-                  <a href="#">Google maps</a>
-                </li>
-                <li>
-                  <a href="#">Open street map</a>
-                </li>
-              </ul>
-            </div>
-          </li>
-          <li class="header-menu">
-            <span>Extra</span>
-          </li>
-          <li>
-            <a href="#">
-              <i class="fa fa-book"></i>
-              <span>Cadastrar Baladas</span>
-              <span class="badge badge-pill badge-primary">Beta</span>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <i class="fa fa-calendar"></i>
-              <span>Calendar</span>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <i class="fa fa-folder"></i>
-              <span>Examples</span>
-            </a>
-          </li>
-        </ul>
-      </div>
+<nav class="navbar navbar-expand-lg text-center">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span > <i class="fas fa-bars"></i> <span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div style="width:100%;">
+      <ul class="navbar-nav" style='justify-content:space-around;'>
+        <li class="nav-item active">
+          <a class="nav-link" href="index.php">Dashboard <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Baladas
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Something else here</a>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Serviços</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="forum.php">Fórum</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Sobre Nós / Depoimentos </a>
+        </li>
+
+      </ul>
     </div>
-  </nav>
-</div>
+  </div>
+</nav>
   
